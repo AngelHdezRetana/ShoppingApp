0733939612997262e5a609c01ab11fc9aeb0cfc9
AngelHdezRetana <angel.hdez.retana@gmail.com>
2024-06-05 13:37:57 -0600

    Initial commit with default app template created

---
+++ ShoppingDemo/NetworkClient.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkClient.swift
+//  ShoppingDemo
+//
+//  Created by Angel Hernández on 31/05/24.
+//
+
+import Foundation
+
+public struct NetworkClient {
+    
+    public static func live() -> NetworkClient {
+        return NetworkClient()
+    }
+}
